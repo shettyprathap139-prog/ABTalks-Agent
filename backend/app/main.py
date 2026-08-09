f4c4ee14b103ada1b0471ccab3a9951b1a0aea95
Prathik Shetty <shettyprathap139@gmail.com>
2026-08-09 12:47:15 +0530

    Configure CORS for public frontend

--- backend/app/main.py
+++ backend/app/main.py
@@ -8,11 +8,8 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "http://127.0.0.1:5500",
-        "http://localhost:5500",
-    ],
-    allow_credentials=True,
+    allow_origins=["*"],
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
